71520d0515f7518cf774d8063bb4b9f796fe9f6b
Abheekrishna <abheekrishna17@gmail.com>
2023-07-19 19:13:12 +0530

    Created gif for loading and ending

--- script.js
+++ script.js
@@ -16,20 +16,36 @@ const getAge = (name) => {
 function createGif() {
     const gif = document.createElement('img');
     gif.src = './images/gif.gif';
+    gif.classList.add('guess');
     outputDiv.appendChild(gif);
+
+    setTimeout(() => {
+        removeImage(gif);
+    }, 2000);
 };
 
-function removeGif(gif) {
-    outputDiv.removeChild(gif);
+function thank() {
+    const dontThank = document.createElement('img');
+    dontThank.src = './images/gif2.gif';
+    dontThank.classList.add('thank');
+    outputDiv.appendChild(dontThank);
+}
+
+
+function removeImage(imageElement) {
+    imageElement.parentNode.removeChild(imageElement);
 }
 
 button.addEventListener('click', () => {
     if(!isNaN(input.value)) {
         output.innerText = `Is this your age? Enter properly`;
     } else {
-        removeGif(createGif())
-        setInterval(() => {
+        createGif()
+        setTimeout(() => {
             getAge(input.value);
-        }, 5000);
+        }, 2000);
+        setTimeout(() => {
+            thank();
+        }, 2000);
     }
 })
